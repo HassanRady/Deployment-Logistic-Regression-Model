eace9d06929ffa868c387a36643bf91a92493af4
Hassan Rady <hassan.khaled.rady@gmail.com>
2021-02-12 10:29:39 +0200

    bugs

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request
-
+from flask import Blueprint, request, jsonify
+from logistic_regression_model.predict import predict as model_predict
 from api.config import get_logger
 
 _logger = get_logger(logger_name=__name__)
@@ -13,3 +13,19 @@ def health():
     if request.method == 'GET':
         _logger.info('health status OK')
         return 'ok'
+
+
+@prediction_app.route('/v1/predict/logistic', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        json_data = request.get_json()
+        _logger.info(f'Inputs: {json_data}')
+
+        result = model_predict(data=json_data)
+        _logger.info(f'Outputs: {result}')
+
+        predictions = int(result.get('predictions')[0])
+        version = result.get('version')
+
+        return jsonify({'predictions': predictions,
+                        'version': version})
