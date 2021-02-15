5c888fa50bb8989d8e0f3a410daf5489b5b2c3af
Hassan Rady <hassan.khaled.rady@gmail.com>
2021-02-15 09:06:29 +0200

    resolving

--- packages/logistic_regression_model/logistic_regression_model/processing/data_management.py
+++ packages/logistic_regression_model/logistic_regression_model/processing/data_management.py
@@ -24,5 +24,19 @@ def save_pipeline(pipeline: Pipeline) -> None:
     save_filename = f"{config.PIPELINE_SAVE_FILE}{_version}.pkl"
     save_path = config.TRAINED_MODEL_DIR / save_filename
 
+    remove_old_pipelines(files_to_keep=save_filename)
     joblib.dump(pipeline, save_path)
     _logger.info(f"Saved Pipeline {save_filename}")
+
+
+def remove_old_pipelines(*, files_to_keep) -> None:
+    """
+    Remove old model pipelines.
+    This is to ensure there is a simple one-to-one
+    mapping between the package version and the model
+    version to be imported and used by other applications.
+    """
+
+    for model_file in config.TRAINED_MODEL_DIR.iterdir():
+        if model_file.name not in [files_to_keep, "__init__.py"]:
+            model_file.unlink()
