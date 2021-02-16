72cd73a6a186c4fef4b599a4153e343ca5c7f6f7
Hassan Rady <hassan.khaled.rady@gmail.com>
2021-02-16 09:10:19 +0200

    differential tests 1

---
+++ packages/ml_api/tests/differential_tests/capture_model_predictions.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from logistic_regression_model.predict import predict as model_predict
+from logistic_regression_model.processing.data_management import load_dataset
+from logistic_regression_model.config import config as model_config
+from api import config
+
+
+def capture_predictions(*args, filename="test_data_predictions"):
+    test_data = load_dataset(model_config.DATA_FILE)
+    test_data = test_data[50:555]
+
+    predictions = model_predict(test_data)
+    predictions_df = pd.DataFrame(predictions)
+
+    predictions_df.to_csv(
+        f"{config.PACKAGE_ROOT.parent}/logistic_regression_model/logistic_regression_model/datasets/{save_file}")
+
+
+
+if __name__ == '__main__':
+    capture_predictions()
